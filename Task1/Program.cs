035cb0f2a65e257e4ac58cd22c367de806b7ba1a
Vetall <Vetall@DESKTOP-B4NE1B5>
2021-04-22 11:46:42 +0300

    TestProject

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Task1
 {
@@ -10,9 +11,24 @@ namespace Task1
             
         }
 
-        public static string Encoder(string str, int rails)
+        public string Encode(string input, int rails)
         {
-            return "text";
+            var result = new string[rails];
+            var trackIdx = 0;
+            var direction = 1;
+            var directionCount = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                result[trackIdx] += input[i];
+                trackIdx += direction;
+                directionCount++;
+                if (directionCount == rails - 1)
+                {
+                    direction *= -1;
+                    directionCount = 0;
+                }
+            }
+            return result.Aggregate("", (x, y) => x + y);
         }
         public static string Decoder(string str, int rails)
         {
